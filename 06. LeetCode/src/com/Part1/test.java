ed8eea2abc94346c5cea6e81e461a944576d5dee
Abhinav Ojha <abhinavojha@gofynd.com>
2024-02-28 00:16:25 +0530

    ID: FRTI-3311: Merge Final Code

---
+++ 06. LeetCode/src/com/Part1/test.java
@@ -0,0 +1,13 @@
+package com.Part1;
+
+public class test {
+    public static void main(String[] args) {
+        int num = 2;
+        String num1 = Integer.toBinaryString(num);
+        int num2 = 3;
+        String num3 = Integer.toBinaryString(num2);
+        System.out.println(num1);
+        System.out.println(num3);
+    }
+    
+}
